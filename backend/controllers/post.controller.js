9b39e1cffd8f636a4bdf22bf3ec38f5b49eb23a1
Kanak Maurya <kanakmaurya22@gmail.com>
2026-06-30 09:52:55 +0530

    add comment route and controllers

--- backend/controllers/post.controller.js
+++ backend/controllers/post.controller.js
@@ -2,6 +2,7 @@ import User from "../models/user.model.js";
 import Profile from "../models/profile.model.js";
 import bcrypt from "bcrypt";
 import Post from "../models/post.model.js";
+import Comment from "../models/comments.model.js";
 
 
 export const activeCheck = async (req, res) => {
@@ -79,3 +80,106 @@ export const deletePost = async (req, res) => {
     });
   }
 }
+
+export const commentPost = async (req, res) => {
+  const { token, post_id, commentBody } = req.body;
+  try{
+
+    const user = await User.findOne({ token }).select("_id ");
+
+    const post = await Post.findOne({ _id: post_id });
+    if(!post){
+      return res.status(404).json({ message: "Post not found" });
+    } 
+
+    const comment = new Comment({
+      userId: user._id,
+      postId: post._id,
+      comment: commentBody,
+    });
+    
+    await comment.save();
+
+    return res.status(201).json({ message: "Comment added successfully" });
+ 
+  }catch(err){
+    return res.status(500).json({
+      error: err.message,
+    });
+  }
+}
+
+export const getCommentsByPost = async (req, res) => {
+  const { post_id } = req.body;
+
+  try{
+
+    const post = await Post.findOne({ _id: post_id });
+    if(!post){
+      return res.status(404).json({ message: "Post not found" });
+    }
+
+    return res.json({comments: post.comments});
+
+
+  }catch(err){
+    return res.status(500).json({
+      error: err.message,
+    });
+  }
+}
+
+export const deleteComment = async (req, res) => {
+  const { token, comment_id } = req.body;
+
+  try{
+
+
+    const user = await User.findOne({ token }).select("_id ");
+
+    if(!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+
+    const comment = await Comment.findOne({"_id": comment_id });
+
+    if(!comment){
+      return res.status(404).json({ message: "Comment not found" });
+    }
+
+    if(comment.userId.toString() !== user._id.toString()){
+      return res.status(403).json({ message: "You are not authorized to delete this comment" });
+    }
+
+    await Comment.deleteOne({ _id: comment_id });
+    return res.status(200).json({ message: "Comment deleted successfully" });
+
+
+  }catch(err){
+    return res.status(500).json({
+      error: err.message,
+    });
+  }
+}
+
+export const incrementLikes = async (req, res) =>{
+  const { post_id} = req.body;
+
+  try{
+
+    const post = await Post.findOne({"_id": post_id});
+
+    if(!post){
+      return res.status(404).json({ message: "Post not found" });
+    }
+
+    post.likes = post.likes + 1;
+    await post.save();
+    return res.json({message: "Likes incremented"});
+  }catch(err){
+    return res.status(500).json({
+      error: err.message,
+    });
+  }
+}
+
--- backend/routes/post.routes.js
+++ backend/routes/post.routes.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { activeCheck } from "../controllers/post.controller.js";
+import { activeCheck, commentPost, deleteComment, getCommentsByPost, incrementLikes } from "../controllers/post.controller.js";
 import { createPost } from "../controllers/post.controller.js";
 import multer from "multer";
 import { getAllPosts } from "../controllers/post.controller.js";
@@ -21,5 +21,10 @@ router.route("/").get(activeCheck)
 router.route("/post").post(upload.single('media'), createPost);
 router.route("/posts").get(getAllPosts);
 router.route("/delete_post").post(deletePost);
+router.route("/comment").post(commentPost);
+router.route("/get_comment").get(getCommentsByPost);
+router.route("/delete_comment").delete(deleteComment);
+router.route("/increment_likes").post(incrementLikes);
+
 
 export default router;
